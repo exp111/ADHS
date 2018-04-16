57186a17e949322790ee7ed1e30f549a68ca7e8d
T N <theexp111@gmail.com>
2018-04-16 14:22:21 +0200

    Added Day, Month, Year

--- P1c/tweet.h
+++ P1c/tweet.h
@@ -10,6 +10,7 @@ class tweet
 private:
 	std::string text;
 	int hour, min, sec;
+	int day, month, year;
 public:
 	tweet();
 	tweet(std::string text);
@@ -28,6 +29,9 @@ tweet::tweet(std::string text)
 	hour = ltm->tm_hour;
 	min = ltm->tm_min;
 	sec = ltm->tm_sec;
+	day = ltm->tm_mday;
+	month = ltm->tm_mon;
+	year = ltm->tm_year;
 	this->text = text;
 }
 
@@ -37,7 +41,7 @@ tweet::~tweet()
 
 std::ostream & operator << (std::ostream  & stream, const tweet & _tweet)
 {
-	stream << std::setw(2) << std::setfill('0') << _tweet.hour << ":" <<_tweet.min << ":" << _tweet.sec << " -> " << _tweet.text;
+	stream << std::setw(2) << std::setfill('0') << _tweet.hour << ":" <<_tweet.min << ":" << _tweet.sec << " " << _tweet.day << ":" << _tweet.month +1 << ":" << _tweet.year + 1900 << " -> " << _tweet.text;
 	return stream;
 }
 
